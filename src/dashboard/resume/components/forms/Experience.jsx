07478495c2bf81637279c94c27d9b808039dd635
sarka99 <119513443+sarka99@users.noreply.github.com>
2024-09-22 11:50:57 +0200

    fixing import errror

--- src/dashboard/resume/components/forms/Experience.jsx
+++ src/dashboard/resume/components/forms/Experience.jsx
@@ -1,7 +1,7 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import React, { useContext, useEffect, useState } from 'react'
-import RichTextEditor from '../RichTextEditor'
+import RichTextEditor from '../../../components/RichTextEditor';
 import { ResumeInfoContext } from '@/context/ResumeInfoContext'
 import { useParams } from 'react-router-dom'
 import GlobalApi from './../../../../../service/GlobalApi'
